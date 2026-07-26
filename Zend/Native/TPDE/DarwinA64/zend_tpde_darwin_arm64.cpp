75b77c333df779f42d3c2ab30cb15cab0d047a1a
Johann-Peter Hartmann <johann-peter.hartmann@mayflower.de>
2026-07-26 22:53:49 +0200

    perf(native): fold string length frame accesses

--- Zend/Native/TPDE/DarwinA64/zend_tpde_darwin_arm64.cpp
+++ Zend/Native/TPDE/DarwinA64/zend_tpde_darwin_arm64.cpp
@@ -2995,16 +2995,10 @@ bool ZendCompilerA64::compile_inst(
 	auto string_length = [&]() {
 		zend_tpde_string_length layout;
 
-		if (!zend_tpde_string_length_at(mir, &layout)) {
+		if (!zend_tpde_string_length_at(mir, &layout)
+				|| layout.operand_offset > UINT32_MAX - 8
+				|| layout.result_offset > UINT32_MAX - 8) {
 			return execute_value_operation();
-		}
-		for (auto reg_id : register_file.used_regs()) {
-			::tpde::Reg reg{reg_id};
-			if (!register_file.is_fixed(reg)
-					&& register_file.reg_local_idx(reg)
-						!= INVALID_VAL_LOCAL_IDX) {
-				evict_reg(reg);
-			}
 		}
 		auto slow = text_writer.label_create();
 		auto done = text_writer.label_create();
@@ -3012,46 +3006,51 @@ bool ZendCompilerA64::compile_inst(
 			val_ref_single(IRValueRef{Adaptor::FRAME_VALUE});
 		auto frame_scratch = std::move(frame).into_scratch();
 		auto frame_reg = frame_scratch.cur_reg();
-		ScratchReg slot{this};
 		ScratchReg type{this};
 		ScratchReg string{this};
-		auto slot_reg = slot.alloc_gp();
 		auto type_reg = type.alloc_gp();
 		auto string_reg = string.alloc_gp();
 
-		add_unsigned_offset(slot_reg, frame_reg, layout.operand_offset);
-		load_off(type_reg, slot_reg,
-			static_cast<uint32_t>(offsetof(zval, u1.type_info)), 4);
+		load_off(type_reg, frame_reg,
+			layout.operand_offset
+				+ static_cast<uint32_t>(offsetof(zval, u1.type_info)),
+			4);
 		ASM(ANDwi, type_reg, type_reg, Z_TYPE_MASK);
 		ASM(CMPwi, type_reg, IS_STRING);
 		generate_raw_jump(Jump::Jne, slow);
-		load_off(string_reg, slot_reg, 0, 8);
+		load_off(string_reg, frame_reg, layout.operand_offset, 8);
 
-		add_unsigned_offset(slot_reg, frame_reg, layout.result_offset);
-		load_off(type_reg, slot_reg,
-			static_cast<uint32_t>(offsetof(zval, u1.type_info)), 4);
+		load_off(type_reg, frame_reg,
+			layout.result_offset
+				+ static_cast<uint32_t>(offsetof(zval, u1.type_info)),
+			4);
 		ASM(TSTwi, type_reg,
 			IS_TYPE_REFCOUNTED << Z_TYPE_FLAGS_SHIFT);
 		generate_raw_jump(Jump::Jne, slow);
 		load_off(string_reg, string_reg,
 			static_cast<uint32_t>(offsetof(zend_string, len)), 8);
-		store_off(slot_reg, 0, string_reg, 8);
+		store_off(frame_reg, layout.result_offset, string_reg, 8);
 		materialize_constant(
 			IS_LONG, DarwinConfig::GP_BANK, 4, type_reg);
-		store_off(slot_reg,
-			static_cast<uint32_t>(offsetof(zval, u1.type_info)),
+		store_off(frame_reg,
+			layout.result_offset
+				+ static_cast<uint32_t>(offsetof(zval, u1.type_info)),
 			type_reg, 4);
 		generate_raw_jump(Jump::jmp, done);
 
 		label_place(slow);
-		slot.reset();
 		type.reset();
 		string.reset();
+		const auto register_state =
+			zend::native::tpde::
+				capture_conditional_call_register_state(*this);
 		ValuePart frame_argument{DarwinConfig::GP_BANK, 8};
 		frame_argument.set_value(this, std::move(frame_scratch));
 		if (!execute_value_operation(&frame_argument)) {
 			return false;
 		}
+		zend::native::tpde::restore_conditional_call_register_state(
+			*this, register_state);
 		label_place(done);
 		return true;
 	};
--- Zend/Native/TPDE/LinuxX64/zend_tpde_linux_x64.cpp
+++ Zend/Native/TPDE/LinuxX64/zend_tpde_linux_x64.cpp
@@ -3211,17 +3211,9 @@ register_operand:
 		zend_tpde_string_length layout;
 
 		if (!zend_tpde_string_length_at(mir, &layout)
-				|| layout.operand_offset > INT32_MAX
-				|| layout.result_offset > INT32_MAX) {
+				|| layout.operand_offset > INT32_MAX - 8
+				|| layout.result_offset > INT32_MAX - 8) {
 			return execute_value_operation();
-		}
-		for (auto reg_id : register_file.used_regs()) {
-			tpde::Reg reg{reg_id};
-			if (!register_file.is_fixed(reg)
-					&& register_file.reg_local_idx(reg)
-						!= INVALID_VAL_LOCAL_IDX) {
-				evict_reg(reg);
-			}
 		}
 		auto slow = text_writer.label_create();
 		auto done = text_writer.label_create();
@@ -3229,31 +3221,26 @@ register_operand:
 			val_ref_single(IRValueRef{Adaptor::FRAME_VALUE});
 		auto frame_scratch = std::move(frame).into_scratch();
 		auto frame_reg = frame_scratch.cur_reg();
-		ScratchReg slot{this};
 		ScratchReg type{this};
 		ScratchReg string{this};
-		auto slot_reg = slot.alloc_gp();
 		auto type_reg = type.alloc_gp();
 		auto string_reg = string.alloc_gp();
 
-		ASM(MOV64rr, slot_reg, frame_reg);
-		ASM(ADD64ri, slot_reg,
-			static_cast<int32_t>(layout.operand_offset));
 		ASM(MOV32rm, type_reg,
-			FE_MEM(slot_reg, 0, FE_NOREG,
-				static_cast<int32_t>(offsetof(zval, u1.type_info))));
+			FE_MEM(frame_reg, 0, FE_NOREG,
+				static_cast<int32_t>(
+					layout.operand_offset + offsetof(zval, u1.type_info))));
 		ASM(AND32ri, type_reg, Z_TYPE_MASK);
 		ASM(CMP32ri, type_reg, IS_STRING);
 		generate_raw_jump(Jump::jne, slow);
 		ASM(MOV64rm, string_reg,
-			FE_MEM(slot_reg, 0, FE_NOREG, 0));
+			FE_MEM(frame_reg, 0, FE_NOREG,
+				static_cast<int32_t>(layout.operand_offset)));
 
-		ASM(MOV64rr, slot_reg, frame_reg);
-		ASM(ADD64ri, slot_reg,
-			static_cast<int32_t>(layout.result_offset));
 		ASM(MOV32rm, type_reg,
-			FE_MEM(slot_reg, 0, FE_NOREG,
-				static_cast<int32_t>(offsetof(zval, u1.type_info))));
+			FE_MEM(frame_reg, 0, FE_NOREG,
+				static_cast<int32_t>(
+					layout.result_offset + offsetof(zval, u1.type_info))));
 		ASM(TEST32ri, type_reg,
 			IS_TYPE_REFCOUNTED << Z_TYPE_FLAGS_SHIFT);
 		generate_raw_jump(Jump::jne, slow);
@@ -3261,23 +3248,30 @@ register_operand:
 			FE_MEM(string_reg, 0, FE_NOREG,
 				static_cast<int32_t>(offsetof(zend_string, len))));
 		ASM(MOV64mr,
-			FE_MEM(slot_reg, 0, FE_NOREG, 0), string_reg);
+			FE_MEM(frame_reg, 0, FE_NOREG,
+				static_cast<int32_t>(layout.result_offset)),
+			string_reg);
 		ASM(MOV32mi,
-			FE_MEM(slot_reg, 0, FE_NOREG,
-				static_cast<int32_t>(offsetof(zval, u1.type_info))),
+			FE_MEM(frame_reg, 0, FE_NOREG,
+				static_cast<int32_t>(
+					layout.result_offset + offsetof(zval, u1.type_info))),
 			IS_LONG);
 		generate_raw_jump(Jump::jmp, done);
 
 		label_place(slow);
-		slot.reset();
 		type.reset();
 		string.reset();
+		const auto register_state =
+			zend::native::tpde::
+				capture_conditional_call_register_state(*this);
 		ValuePart frame_argument{
 			tpde::x64::PlatformConfig::GP_BANK, 8};
 		frame_argument.set_value(this, std::move(frame_scratch));
 		if (!execute_value_operation(&frame_argument)) {
 			return false;
 		}
+		zend::native::tpde::restore_conditional_call_register_state(
+			*this, register_state);
 		label_place(done);
 		return true;
 	};
